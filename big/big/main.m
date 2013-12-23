aeb3cd962e9342af702aadfb5f46ad3b777398a5
BSA univ 16 <bsauniv16@BSAs-iMac-26.local>
2013-12-23 16:29:21 +0530

    i changed

---
+++ big/big/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  big
+//
+//  Created by BSA univ 16 on 19/12/13.
+//  Copyright (c) 2013 BSA univ 16. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "largr.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        largr *apple=[[largr alloc]init];
+        [apple setto:2 :3:5];
+        
+        
+            }
+    return 0;
+}
+
